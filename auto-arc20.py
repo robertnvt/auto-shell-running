0f0d2a2406fcd1b7d7a6148715fb2cc7ec1ed8f7
robertnvt <125644595+robertnvt@users.noreply.github.com>
2023-12-31 00:40:43 +0800

    Update auto-arc20.py
    solve following issue:
    UnicodeDecodeError: 'gbk' codec can't decode byte 0x84 in position 104: illegal multibyte sequence

--- auto-arc20.py
+++ auto-arc20.py
@@ -3,13 +3,13 @@ import time
 
 def execute_command(command):
     try:
-        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
+        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         while True:
             output = process.stdout.readline()
-            if output == '' and process.poll() is not None:
+            if output == b'' and process.poll() is not None:
                 break
             if output:
-                print(output.strip())
+                print(output.decode('gbk').strip())
 
         return process.poll()
     except subprocess.CalledProcessError as e:
@@ -17,9 +17,9 @@ def execute_command(command):
         return None
 
 # Example usage
-command = "yarn cli mint-dft higgsboson --satsbyte=80"  # Replace with your desired command
+command = "dir"  # Replace with your desired command
 num_repeats = 5  # Number of times to repeat the command
-retry_delay = 3  # Delay in seconds between command executions
+retry_delay = 10  # Delay in seconds between command executions
 
 for i in range(num_repeats):
     print(f"Executing command: {command}")
